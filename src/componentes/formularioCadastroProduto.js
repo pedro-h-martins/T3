8cc718a6acca8255dc17fa8f2fea7d7efbe3a482
pedro-h-martins <pedrohenrimartinss@gmail.com>
2025-06-18 10:26:31 -0300

    feat: adicionar novos dados para o FormularioCadastroServico

--- src/componentes/formularioCadastroProduto.js
+++ src/componentes/formularioCadastroProduto.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useRef } from "react";
 
 export default function FormularioCadastroProduto(props) {
+    const formRef = useRef(null);
+
+    const handleReset = (e) => {
+        e.preventDefault();
+        if (formRef.current) {
+            formRef.current.reset();
+        }
+    }
+
     let tema = props.tema;
-    
     return (
-        <div className="container">
-            <h3 className="text-center">Formulário de Cadastro de Produto</h3>
+        <div className="container-fluid">
+            <form ref={formRef} onSubmit={handleReset}>
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="Nome do produto" aria-label="Nome do produto" aria-describedby="Nome do produto" />
+                </div>
+                <div className="input-group mb-3">
+                    <span className="input-group-text">R$</span>
+                    <input type="number" step="0.01" min="0" className="form-control" placeholder="Preço do produto" aria-label="Preço do produto" aria-describedby="Preço do produto" />
+                </div>
+                <div className="input-group mb-3">
+                    <button className="btn btn-outline-secondary" type="submit" style={{ background: tema }}>Cadastrar</button>
+                </div>
+            </form>
         </div>
     );
 };
--- src/componentes/formularioCadastroServico.js
+++ src/componentes/formularioCadastroServico.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 
-export default  function FormularioCadastroServico(props) {
+export default function FormularioCadastroServico(props) {
+    const formRef = useRef();
     const tema = props.tema;
-    
+
+    const handleReset = (e) => {
+        e.preventDefault();
+        if (formRef.current) {
+            formRef.current.reset();
+        }
+    }
+
     return (
-        <div className="container">
-            <h3 className="text-center">Formulário de Cadastro de Serviço</h3>
+        <div className="container-fluid">
+            <form ref={formRef} onSubmit={handleReset}>
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="Nome do serviço" aria-label="Nome do serviço" aria-describedby="Nome do serviço" />
+                </div>
+                <div className="input-group mb-3">
+                    <span className="input-group-text">R$</span>
+                    <input type="number" step="0.01" min="0" className="form-control" placeholder="Preço do serviço" aria-label="Preço do serviço" aria-describedby="Preço do serviço" />
+                </div>
+                <div className="input-group mb-3">
+                    <button className="btn btn-outline-secondary" type="submit" style={{ background: tema }}>Cadastrar</button>
+                </div>
+            </form>
         </div>
     );
-};
+}
